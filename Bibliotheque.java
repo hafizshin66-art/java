aae454fc2cb0e9149d412abc56ffaf6900d82422
bin ameer wahdi adam <ba507149@rtbase2lin.campus.unice.fr>
2026-09-21 15:18:27 +0200

    bieeeb

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -1,8 +1,24 @@
+import java.util.ArrayList;
 public class Bibliotheque {
     public String nom;
+    public ArrayList<Livre> livres;
 
-    public Bibliotheque(String nom){
-        this.nom=nom;
+    public Bibliotheque(String nom) {
+        this.nom = nom;
+        this.livres = new ArrayList<Livre>();
     }
 
+    public void ajouterLivre(Livre l) {
+        livres.add(l);
+    }
+
+    public void supprimerLivre(Livre l) {
+        livres.remove(l);
+    }
+    public void affichageListe() {
+        System.out.println("Liste des livres dans la bibliothèque " + nom + ":");
+        for(Livre l : livres) {
+            l.afficher();
+        }
+    }
 }
