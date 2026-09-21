61c7de527da592cca29f21086560b7d83654a23c
bin ameer wahdi adam <ba507149@rtbase2lin.campus.unice.fr>
2026-09-21 15:08:39 +0200

    bib

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -1,3 +1,8 @@
 public class Bibliotheque {
+    public String nom;
+
+    public Bibliotheque(String nom){
+        this.nom=nom;
+    }
 
 }
--- Livre.java
+++ Livre.java
@@ -21,4 +21,5 @@ public class Livre {
     public int getAnneePublication() {
         return anneePublication;
     }
+
 }
