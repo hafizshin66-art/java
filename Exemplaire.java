c4195cd99d44db3d3b8613437d151c132f0dd274
bin ameer wahdi adam <ba507149@rtbase2lin.campus.unice.fr>
2026-09-25 13:11:12 +0200

    bieedegfdfdfefefefez

--- Exemplaire.java
+++ Exemplaire.java
@@ -1,3 +1,21 @@
 public class Exemplaire {
+    public String etat;
+    public String emplacement;
 
+    public Exemplaire(String etat, String emplacement){
+        this.etat = etat;
+        this.emplacement = emplacement;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public String getEmplacement() {
+        return emplacement;
+    }
+
+    public void afficher(){
+        System.out.println("Etat: "+this.getEtat()+" |Emplacement: "+this.getEmplacement());
+    }
 }
